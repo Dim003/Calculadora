fc55375759fa9f0586d26282aa1cf577a10e1e5f
Fernando Zocarato <fernandozocarato@hotmail.com>
2022-10-17 21:25:30 -0300

    att

--- Calculadora/Assets/Scripts/CalculadoraController.cs
+++ Calculadora/Assets/Scripts/CalculadoraController.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Introducao : MonoBehaviour
+public class CalculadoraController : MonoBehaviour
 {
+
+    [SerializeField] private Text TextDisplayI; 
+    [SerializeField] private Text TextDisplayII; 
 
     void Start()
     {
+        TextDisplayI.text = "";
+        TextDisplayII.text = "";
     }
 
-    // Update is called once per frame
     void Update()
     {
 
     }
+
+    public void BtnChacaracter(string character) 
+    {
+        TextDisplayII.text = character;
+        TextDisplayI.text = TextDisplayI.text + TextDisplayII.text;
+    }
+    
 }
